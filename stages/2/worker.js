5121e76c036c18b2318425d06512de48497d32d0
Jacek Kopecky <jacek@jacek.cz>
2021-04-27 16:21:56 +0100

    empty starting point

--- stages/2/worker.js
+++
@@ -1,6 +0,0 @@
-/* Installing the service worker */
-function installMyServiceWorker() {
-  console.log('service worker installer code running');
-}
-
-self.addEventListener('install', installMyServiceWorker);
